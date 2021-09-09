4c8c56ff7b481b90922c173815638ab24669faf8
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-09-09 10:25:35 -0400

    added build script for wasm

---
+++ image-processing/build.sh
@@ -0,0 +1,6 @@
+wasm-pack build --out-dir ../frontend/src/wasm
+cd ../frontend/src/wasm
+rm ./.gitignore ./package.json
+echo -e "/* eslint-disable */\n$(cat ./image_processing_bg.js)" > image_processing_bg.js
+sed -i 1d ./image_processing.js
+echo "build complete"
