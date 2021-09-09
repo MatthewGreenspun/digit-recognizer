01f2b22f2783586417c4d3871ba15c7a37484464
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-09-09 17:14:37 -0400

    added find_image_boundaries rust function

--- image-processing/src/lib.rs
+++ image-processing/src/lib.rs
@@ -35,7 +35,7 @@ pub fn gray_scale_image(data: web_sys::ImageData) -> js_sys::Float32Array {
 }
 
 #[wasm_bindgen]
-pub fn row_is_black(data: web_sys::ImageData, row: u32) -> bool {
+pub fn row_is_black(data: &web_sys::ImageData, row: u32) -> bool {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     let width: u32 = data.width() * 4;
     let pixel_data: Vec<u8> = data.data().0;
@@ -54,7 +54,7 @@ pub fn row_is_black(data: web_sys::ImageData, row: u32) -> bool {
 }
 
 #[wasm_bindgen]
-pub fn col_is_black(data: web_sys::ImageData, col: u32) -> bool {
+pub fn col_is_black(data: &web_sys::ImageData, col: u32) -> bool {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     let width: u32 = data.width() * 4;
     let height: u32 = data.height();
@@ -71,4 +71,64 @@ pub fn col_is_black(data: web_sys::ImageData, col: u32) -> bool {
         i += width;
     }
     true
+}
+
+#[wasm_bindgen]
+pub fn find_image_boundaries(data: &web_sys::ImageData, square: bool) -> js_sys::Array {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    let width = data.width() as i32;
+    let height = data.height() as i32;
+
+    //top, bottom, left, and right boundaries of the digit
+    let mut top: i32 = -1;
+    let mut bottom: i32 = height - 1;
+    let mut left: i32 = -1;
+    let mut right: i32 = width - 1;
+
+    let mut prev_row_is_black = row_is_black(&data, 0);
+    let mut i = 0;
+    while i < height {
+        let curr_row_is_black = row_is_black(&data, i as u32);
+        if !curr_row_is_black && top == -1 {
+            top = i;
+        } else if i != 0 && !prev_row_is_black && curr_row_is_black && top != -1 {
+            bottom = i;
+        }
+        i = i + 1;
+        prev_row_is_black = curr_row_is_black;
+    }
+
+    let mut prev_col_is_black = col_is_black(&data, 0);
+    i = 0;
+    while i < width {
+        let curr_col_is_black = col_is_black(&data, i as u32);
+        if !curr_col_is_black && left == -1 {
+            left = i;
+        } else if i != 0 && !prev_col_is_black && curr_col_is_black && left != -1 {
+            right = i;
+        }
+        i = i + 1;
+        prev_col_is_black = curr_col_is_black;
+    }
+
+    let js_arr = js_sys::Array::new_with_length(4);
+    if top == -1 || left == -1 {
+        //never found any white pixels so just return the entire canvas
+        js_arr.set(0, JsValue::from(0));
+        js_arr.set(1, JsValue::from(0));
+        js_arr.set(2, JsValue::from(width));
+        js_arr.set(3, JsValue::from(height));
+    } else if square {
+        let square_length = std::cmp::max(right - left, bottom - top);
+        js_arr.set(0, JsValue::from(left));
+        js_arr.set(1, JsValue::from(top));
+        js_arr.set(2, JsValue::from(square_length));
+        js_arr.set(3, JsValue::from(square_length));
+    } else {
+        js_arr.set(0, JsValue::from(left));
+        js_arr.set(1, JsValue::from(top));
+        js_arr.set(2, JsValue::from(right-left));
+        js_arr.set(3, JsValue::from(bottom-top));
+    }
+    js_arr
 }
